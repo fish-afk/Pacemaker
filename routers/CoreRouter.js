993d72e2aba0b5cafd9a93ff36c10c744cafcf9e
fish-afk <mirzashihab2@gmail.com>
2023-08-31 19:58:25 +0200

    files sorted

--- routers/CoreRouter.js
+++ routers/CoreRouter.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const Module = require('./module')
+const Module = require('../modules/CoreModule')
 
 router.get('/getcmd', Module.GETCMD);
 router.get('/postresult', Module.recieve_result);
